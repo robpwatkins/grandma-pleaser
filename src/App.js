56f6405baeb12391351dab074005454ad5a5c0f3
robpwatkins <robpwatkins@gmail.com>
2020-12-19 22:15:59 -0600

    god its just so much easier now

--- src/App.js
+++ src/App.js
@@ -4,10 +4,31 @@ import Flame from './components/Flame';
 function App() {
   return (
     <div className="App">
-      <div className="group left"></div>
-      <div className="group bottom"></div>
-      <div className="group right"></div>
-      <div className="group top"></div>
+      <div className="group left">
+        <Flame />
+        <Flame />
+        <Flame />
+        <Flame />
+        <Flame />
+      </div>
+      <div className="group bottom">
+        <Flame />
+        <Flame />
+        <Flame />
+        <Flame />
+      </div>
+      <div className="group right">
+        <Flame />
+        <Flame />
+        <Flame />
+        <Flame />
+      </div>
+      <div className="group top">
+        <Flame />
+        <Flame />
+        <Flame />
+        <Flame />
+      </div>
       <div className="group middle"></div>
     </div>
   );
