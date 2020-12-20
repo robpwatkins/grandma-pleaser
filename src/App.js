6153e9f2cfefa5d1f6d6645039e6f350a4a3737a
Rob Watkins <robwatkins@Admins-MBP.attlocal.net>
2020-12-19 21:39:35 -0600

    starting with the layout

--- src/App.js
+++ src/App.js
@@ -1,9 +1,14 @@
 import './App.css';
+import Flame from './components/Flame';
 
 function App() {
   return (
     <div className="App">
-      
+      <Flame />
+      <Flame />
+      <Flame />
+      <Flame />
+      <Flame />
     </div>
   );
 }
