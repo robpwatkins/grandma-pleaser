14f67b357197a3e224bbe83a1e6b93f5c0ae9135
robpwatkins <robpwatkins@gmail.com>
2020-12-23 12:51:30 -0600

    i fixed ittttt

--- src/components/Video.js
+++ src/components/Video.js
@@ -3,7 +3,7 @@ import React from 'react';
 
 export default function Video() {
   return (
-    <div className="video-test">
+    <div className="video-container">
       <video 
         controls
         autoPlay
