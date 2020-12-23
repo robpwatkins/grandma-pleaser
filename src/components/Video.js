50f2d8667c3e291076e552c3ea2779600617762e
robpwatkins <robpwatkins@gmail.com>
2020-12-23 12:50:13 -0600

    did i fixed it?

--- src/components/Video.js
+++ src/components/Video.js
@@ -1,13 +1,17 @@
 import React from 'react';
+// import Vid from '../video/GrandmaPleaser2020(Unfinished).mov';
 
 export default function Video() {
   return (
     <div className="video-container">
-      <iframe 
-        src="https://drive.google.com/file/d/1JdHUhi7F00w41uM0eGP1URS0UU_SX7Ye/preview" 
-        width="100%" 
-        height="100%">
-      </iframe>
+      <video 
+        controls
+        autoPlay
+        loop
+        // src={Vid}
+        onTimeUpdate={event => console.log(event)}
+      >  
+      </video>
     </div>
   )
 }
