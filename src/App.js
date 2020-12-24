75db8628146b21b37ec68fde54cc18ae84e57d48
robpwatkins <robpwatkins@gmail.com>
2020-12-24 13:10:28 -0600

    its like, almost there

--- src/App.js
+++ src/App.js
@@ -11,34 +11,118 @@ function App() {
   return (
     <div className="App">
       <div className="group left">
-        <Flame setPosition={setPosition} t={0.001} name="Play from Start" />
-        <Flame setPosition={setPosition} t={5} name="Katina" />
-        <Flame setPosition={setPosition} t={25} name="Kay & Gary" />
-        <Flame setPosition={setPosition} t={45} name="The Alexanders" />
+        <Flame 
+          name="Play from Start"
+          t={0.001}
+          setPosition={setPosition} 
+          currentTime={currentTime}
+          window={[0, 4.99]}
+        />
+        <Flame 
+          name="Katina" 
+          t={5} 
+          setPosition={setPosition} 
+          currentTime={currentTime}
+          window={[5, 9.99]}
+        />
+        <Flame 
+          name="Kay & Gary" 
+          t={10} 
+          setPosition={setPosition} 
+          currentTime={currentTime}
+          window={[10, 14.99]}
+        />
+        <Flame 
+          name="The Alexanders" 
+          t={15} 
+          setPosition={setPosition} 
+          currentTime={currentTime}
+          window={[15, 19.99]}
+        />
       </div>
       <div className="group bottom">
-        <Flame setPosition={setPosition} name="The Lewises" />
-        <Flame setPosition={setPosition} name="James & Tricia" />
-        <Flame setPosition={setPosition} name="Al & Lea" />
+        <Flame 
+          name="The Lewises" 
+          t={20} 
+          setPosition={setPosition} 
+          currentTime={currentTime}
+          window={[20, 24.99]}
+        />
+        <Flame 
+          name="James & Tricia" 
+          t={25} 
+          setPosition={setPosition} 
+          currentTime={currentTime}
+          window={[25, 29.99]}
+        />
+        <Flame 
+          name="Al & Lea" 
+          t={30} 
+          setPosition={setPosition} 
+          currentTime={currentTime}
+          window={[30, 34.99]}
+        />
       </div>
       <div className="group right">
-        <Flame setPosition={setPosition} name="Paul & Sallie" />
-        <Flame setPosition={setPosition} name="Rob & Eileen" />
-        <Flame setPosition={setPosition} name="The Irbys" />
+        <Flame 
+          name="Paul & Sallie" 
+          t={45} 
+          setPosition={setPosition} 
+          currentTime={currentTime}
+          window={[45, 49.99]}
+        />
+        <Flame 
+          name="Rob & Eileen" 
+          t={40} 
+          setPosition={setPosition} 
+          currentTime={currentTime}
+          window={[40, 44.99]}
+        />
+        <Flame 
+          name="The Irbys" 
+          t={35} 
+          setPosition={setPosition} 
+          currentTime={currentTime}
+          window={[35, 39.99]}
+        />
         <a 
           href="https://drive.google.com/uc?export=download&id=1tTgmiGSspvrHj81QJ6I0BFnsF5RP85uj"
           target="_none"
         >
-          <Flame setPosition={setPosition} name="Download From Google Drive" />
+          <Flame 
+            setPosition={setPosition} 
+            name="Download From Google Drive" 
+          />
         </a>
       </div>
       <div className="group top">
-        <Flame setPosition={setPosition} name="The Andersons" />
-        <Flame setPosition={setPosition} name="The Hernandezes" />
-        <Flame setPosition={setPosition} name="The Hewitts" />
+        <Flame 
+          name="The Andersons" 
+          t={53} 
+          setPosition={setPosition} 
+          currentTime={currentTime}
+          window={[53, 53.99]}
+        />
+        <Flame 
+          name="The Hernandezes" 
+          t={52} 
+          setPosition={setPosition} 
+          currentTime={currentTime}
+          window={[52, 52.99]}
+        />
+        <Flame 
+          name="The Hewitts" 
+          t={50} 
+          setPosition={setPosition} 
+          currentTime={currentTime}
+          window={[50, 51.99]}
+        />
       </div>
       <div className="group middle">
-        <Video position={position} setCurrentTime={setCurrentTime} />
+        <Video 
+          position={position} 
+          setCurrentTime={setCurrentTime} 
+        />
       </div>
     </div>
   );
--- src/components/Flame.js
+++ src/components/Flame.js
@@ -1,24 +1,33 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import candle from '../img/CandlePlaceholder.png';
 
-export default function Flame({ name, t, setPosition }) {
-  const [isHovered, setIsHovered] = useState(false);
+export default function Flame({ name, t, setPosition, currentTime, window }) {
+  // const [isHovered, setIsHovered] = useState(false);
   const [isActive, setIsActive] = useState(false);
+  const [isClicked, setIsClicked] = useState(false);
+
+  useEffect(() => {
+    if (window) {
+      if (currentTime > window[0] && currentTime < window[1]) {
+        setIsActive(true);
+      } else setIsActive(false);
+    }
+  }, [currentTime])
 
   let flameClass = [
     'flame',
-    isHovered && 'hovered',
+    // isHovered && 'hovered',
     isActive && 'active'
   ];
   flameClass = flameClass.join(' ');
   
   return (
-    <div 
-      className={flameClass}
-      onMouseEnter={() => setIsHovered(true)}
-      onMouseLeave={() => setIsHovered(false)}
+    <div
+      style={isActive ? {transform: 'scale(1.15)'} : null}
+      className='flame'
+      // onMouseEnter={() => setIsHovered(true)}
+      // onMouseLeave={() => setIsHovered(false)}
       onClick={() => {
-        setIsActive(!isActive)
         setPosition(t);
         }
       }
